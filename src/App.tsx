e343a804fda2d584be85d1fa2e2707dac0efabd6
unknown <israel56561@gmail.com>
2026-01-22 18:14:09 +0200

    changed the word world to be a capital letter

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello World!
       </h1>
       <h1>Vite + React</h1>
       <div className="card">
