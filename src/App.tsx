1d2c3a52355bedff2f3f7ca5a20b6d6fa4945eed
Davidn0072 <Davidn0072@Gmail.COM>
2026-01-22 21:55:51 +0200

    Initial commit on feature/mongodb-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import {BrowserRouter, Routes , Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Example from './pages/example';
+import MongoDb from './pages/MongoDb';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/example' element={<Example />} />
+        </Routes>
+        <Routes>
+          <Route path='/MongoDb' element={<MongoDb />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/MongoDb.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MongoDb() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Wellcome to MongoDb Page
+        </h1>
+    )
+}
+
+export default MongoDb
