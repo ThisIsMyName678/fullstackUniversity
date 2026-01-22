820a76ff157dbe529cdb2e568666defbc98b8c95
SHIFU <tomer5142200@walla.co.il>
2026-01-22 20:23:26 +0200

    now the last fix

--- src/pages/CssPage.tsx
+++ src/pages/CssPage.tsx
@@ -1,6 +1,8 @@
 function CssPage() {
   return (
-    <h1 className="text-4xl font-extrabold mb-4 text-center">Welcome 👋</h1>
+    <h1 className="text-4xl font-extrabold mb-4 text-center">
+      Welcome to css page 👋
+    </h1>
   );
 }
 
