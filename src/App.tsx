509cc87673843bd90be5135b5f5f1cb25b649e6a
Shahar <ajr2955@gmail.com>
2026-01-22 20:38:16 +0200

    fixed last added new tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes , Route} from 'react-router-dom';
 import Home from './pages/home';
 import Example from './pages/example';
+import WelcomeToTs from './pages/welcome_to_ts';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/example' element={<Example />} />
+          <Route path='/welcome-to-ts' element={<WelcomeToTs />} />
         </Routes>
       </BrowserRouter>
     </>
