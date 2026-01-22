9fb5921bcfcdf24792e5bdcf46874c98d8fedf5a
Oren Tal <oren.tal2@gmail.com>
2026-01-22 20:40:11 +0200

    change the file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes , Route} from 'react-router-dom';
 import Home from './pages/home';
 import Example from './pages/example';
-import Welcome from './pages/welcome';
+import WelcomeExpress from './pages/WelcomeExpress';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/example' element={<Example />} />
-          <Route path='/welcome' element={<Welcome />} />
+          <Route path='/WelcomeExpress' element={<WelcomeExpress />} />
         </Routes>
       </BrowserRouter>
     </>
