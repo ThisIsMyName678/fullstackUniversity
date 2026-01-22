02e13aa3943a62b09b2fbd0813e409dbc8f449e8
Shahar <ajr2955@gmail.com>
2026-01-22 20:22:32 +0200

    added new tsx page

---
+++ src/pages/welcome_to_ts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function WelcomeToTs() {
+    return (
+        <div >
+            <h1>Welcome to TypeScript</h1>
+            <p>This is a basic TSX page component.</p>
+        </div>
+    );
+}
