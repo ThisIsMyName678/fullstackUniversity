4e089da111550a3aefa549fb524ed074090bf46f
boke42 <omrihadash42@gmail.com>
2026-01-22 20:44:34 +0200

    fix Node page and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Example from "./pages/example";
-import WelcomeToNode from "./pages/WelcomeToNode";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Example from './pages/example';
+import WelcomeToNode from './pages/welcomeToNode';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/example" element={<Example />} />
-        <Route path="/welcomeToNode" element={<WelcomeToNode />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/example' element={<Example />} />
+        <Route path='/welcomeToNode' element={<WelcomeToNode />} />
       </Routes>
     </BrowserRouter>
   );
