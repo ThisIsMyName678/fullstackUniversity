45af09aca212809d0c78e060433696aff838eb31
Oren Tal <oren.tal2@gmail.com>
2026-01-22 20:16:39 +0200

    adding basic welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes , Route} from 'react-router-dom';
 import Home from './pages/home';
 import Example from './pages/example';
+import Welcome from './pages/welcome';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/example' element={<Example />} />
+          <Route path='/welcome' element={<Welcome />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Welcome to the Fullstack University!
+        </h1>
+        <p>this is the best place to learn fullstack development</p>
+    </div>
+    
+  )
+}
+
+export default Welcome
